2dc520807d32604143512c5ba6df05960f3221a7
ReactRay <raydiaz1899@gmail.com>
2024-12-25 02:24:26 +0200

    idk

--- store/store.js
+++ store/store.js
@@ -1,17 +1,16 @@
 import { todoReducer } from './reducers/todo.reducer.js'
 import { userReducer } from './reducers/user.reducer.js'
 
-const {createStore, combineReducers, compose} = Redux
+const { createStore, combineReducers, compose } = Redux
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const rootReducer = combineReducers({
-    todoModule: todoReducer,
-    userModule: userReducer
+  todoModule: todoReducer,
+  userModule: userReducer,
 })
-
 
 export const store = createStore(rootReducer, composeEnhancers())
 window.gStore = store
 
-console.log('store.getState()', store.getState())
+console.log('store.getState()', store.getState()) // for the commit
